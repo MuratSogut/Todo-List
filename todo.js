1b4317639320ada3ae5c805919df585b4294a757
MuratSogut <muratsogut2@gmail.com>
2021-10-30 00:40:38 +0300

    first commit

---
+++ todo.js
@@ -0,0 +1,110 @@
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const filter = document.querySelector("#filter");
+const clearButton = document.querySelector("#clear-todos");
+
+eventListeners();
+
+function eventListeners(){
+    form.addEventListener("submit", addTodo);
+    document.addEventListener("DOMContentLoaded", loadAllTodosToUI);
+}
+
+function loadAllTodosToUI(){
+
+    let todos = getTodosFromStorage();
+
+    todos.forEach(function (todos){
+        addTodoToUI(todos);
+
+    })
+ 
+}
+
+function addTodo(e){
+    const newTodo = todoInput.value.trim();
+    
+    if (newTodo === ""){
+        showAlert("danger","Lütfen bir todo girin...");
+    } else {
+        addTodoToUI(newTodo);
+        addTodoToStorage(newTodo);
+        showAlert("success","Todo başarıyla eklendi...");
+    }
+
+    e.preventDefault();    
+}
+
+function getTodosFromStorage(){
+
+    let todos;
+
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+     
+    }
+    return todos;
+}
+
+function addTodoToStorage(newTodo){
+     
+    let todos = getTodosFromStorage(); 
+
+    todos.push(newTodo);
+    console.log(todos);
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+}
+
+function showAlert(type, message) {
+    const alert = document.createElement("div");
+    alert.className = `alert alert-${type}`;
+    alert.textContent = message;
+
+    firstCardBody.appendChild(alert);
+    
+    setTimeout(function(){
+        alert.remove();
+    }, 1000)
+
+    //'<div class="alert alert-primary" role="alert">This is a primary alert—check it out!</div>';
+     
+}
+
+function addTodoToUI(newTodo){
+ 
+    /*
+     <li class="list-group-item d-flex justify-content-between">
+                            Todo 1
+                            <a href = "#" class ="delete-item">
+                                <i class = "fa fa-remove"></i>
+                            </a>
+
+                        </li>
+    */
+
+    // const listItem = document.createElement("li");
+    // listItem.className = "list-group-item d-flex justify-content-between";
+    // listItem.innerHTML = newTodo + '<a href = "#" class="delete-item"><i class = "fa fa-remove"></i></a>';
+
+
+    const listItem = document.createElement("li");
+    listItem.className = "list-group-item d-flex justify-content-between";
+
+    const link = document.createElement("a");
+    link.href = "#";
+    link.className = "delete-item";
+    link.innerHTML = "<i class = 'fa fa-remove'></i>";
+
+    listItem.appendChild(document.createTextNode(newTodo));
+    listItem.appendChild(link);
+    
+    todoList.appendChild(listItem);
+    todoInput.value = "";
+  
+}
